3dd02585a4d9d5fa313a8d155fbed827a07a0ca5
FrankOfficialGD <35267706+FrankOfficialGD@users.noreply.github.com>
2018-06-14 17:45:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', (message) => {
   	}
 });
 client.on('message', (message) => {
-    if (message.content == 'пошел нахуй!') {
+    if (message.content == 'пошёл нахуй!') {
     	message.reply('сам иди, козёл!');
   	}
 });
