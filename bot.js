f0cc826c0a42ab48534d9402280a0e7afe511296
FrankOfficialGD <35267706+FrankOfficialGD@users.noreply.github.com>
2018-06-15 08:57:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('message',(message)) -> {
+bot.on('message', ()) => {
 
 if (message.content == 'да'){
 message.channel.sendMessage('нет');
